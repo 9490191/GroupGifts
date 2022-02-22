fb11a39e546abd840d17063487ca5880a2a4ef34
Jacques Daneau <9490191@colval.qc.ca>
2022-02-22 16:29:23 -0500

    Adding Login activity

---
+++ app/src/main/java/edu/h22_4m6/group_gifts/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package edu.h22_4m6.group_gifts.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.h22_4m6.group_gifts.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
